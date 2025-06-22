eaecc73d2f93e3cf8ff11a592047a5887bdda556
RaniKagga <nagarani.kagga@gmail.com>
2025-06-22 10:42:04 +0530

    added-project-files

---
+++ SampleCICDTestApp/SampleCICDTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCICDTestAppApp.swift
+//  SampleCICDTestApp
+//
+//  Created by K Nagarani on 22/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleCICDTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
